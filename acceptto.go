4e12dd70484687ead7f6798e9d7e2673c742d2ca
Mani Malekmohammadi <mm.mani@gmail.com>
2015-09-03 11:13:47 +0430

    fix comments

--- acceptto.go
+++ acceptto.go
@@ -1,6 +1,4 @@
 /****************
-Developer: Amir Karimi <a.karimi.k@gmail.com>
-Company: Rastinware.ir
 *****************
 GETING STARTED
 =================
@@ -11,6 +9,9 @@ GETING STARTED
     * go get "github.com/go-martini/martini"
     * go get "github.com/martini-contrib/render"
 * Run the program using this command: go run acceptto.go
+* Open your browser and go to http://localhost:3000
+* Enter your Acceptto email and click on submit
+* Accept or Decline the authentication request on your mobile device
 
 *****************/
 
